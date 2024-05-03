b97a05f4067e80c885ae2a8721a97ae6177ef0c2
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-05-03 19:55:24 +0200

    Working after trying on empty dir 3 - npm install before deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ whereis dfx
 echo $PATH
 /home/Matiki/.local/share/dfx/bin/dfx start --background --clean --host 0.0.0.0:4943
 cd d
+npm install
 /home/Matiki/.local/share/dfx/bin/dfx deploy
 /bin/bash
